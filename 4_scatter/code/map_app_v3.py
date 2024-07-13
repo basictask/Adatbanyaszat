e7c3b6710f03d242558bca5651dbcf4a867780ba
basictask <daniel.kuknyo@mailbox.org>
2024-07-13 21:46:18 +0200

    finished debugging map_app_v3

---
+++ 4_scatter/code/map_app_v3.py
@@ -0,0 +1,71 @@
+import os
+import warnings
+import map_app_v2
+import pandas as pd
+import plotly.express as px
+from pandas_datareader import wb
+import dash_bootstrap_components as dbc
+from map_app_v1 import multiline_indicator
+from dash import Dash, dcc, html, Input, Output, State
+from map_app_v2 import countries, indicators, update_wb_data
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+series = pd.read_csv(os.path.join(current_dir, '../../data/PovStatsSeries.csv'), low_memory=False)
+poverty = pd.read_csv(os.path.join(current_dir, '../../data/poverty.csv'), low_memory=False)
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = map_app_v2.app.layout.children + [  # Előző alkalmazás layout komponense
+    # Új komponensek hozzáadása az előző alkalmazás elrendezésén túl
+    html.Div(style={'marginBottom': '20px'}),  # Vertikális hely a két komponens között
+    dcc.Dropdown(
+        id='indicator_dropdown',
+        value='GINI index (World Bank estimate)',
+        options=[{'label': indicator, 'value': indicator} for indicator in poverty.columns[3:54]]
+    ),
+    dbc.Row(
+        dbc.Col(
+            [
+                dcc.Markdown(
+                    id='indicator_map_details_md',
+                )
+            ],
+            width=12,
+        )
+    ),
+]
+
+
+# Előző alkalmazás callback függvényei
+map_app_v2.register_callbacks(app)
+
+
+@app.callback(
+    Output('indicator_map_details_md', 'children'),
+    Input("indicator_dropdown", "value"),
+)
+def update_indicator_map_details_md(indicator):
+    """
+    Frissíti a MarkDown komponens értékét a legördülő menü állapotának változására
+    """
+    # Adatkészlet beolvasása
+    series_df = series[series['Indicator Name'].eq(indicator)]
+    if series_df.empty:
+        markdown = "Nincs elérhető adat az indikátorról"
+    else:
+        limitations = series_df['Limitations and exceptions'].fillna('N/A').str.replace('\n\n', '').values[0]
+        # Markdown komponens szövegének definiálása
+        markdown = f"""
+            ## {series_df['Indicator Name'].values[0]}
+            {series_df['Long definition'].values[0]}
+            * **Mértékegység:** {series_df['Unit of measure'].fillna('count').values[0]}
+            * **Periodicitás:** {series_df['Periodicity'].fillna('N/A').values[0]}
+            * **Forrás:** {series_df['Source'].values[0]}
+            ### Limitációk és kivételek: 
+            {limitations}
+        """
+    return markdown
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
